fd88de6f61fca2dc0260baff6a486a552a2656e4
Andres Laguilavo <lagui2003@gmail.com>
2023-03-16 12:26:42 -0500

    add prettier-plugin-tailwindcss

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'space-before-function-paren': 'off'
+    'space-before-function-paren': 'off',
+    'react/prop-types': 'off'
   }
 }
