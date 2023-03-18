66ba26ca31deb7f618fc2291e45bef1f07eda9bd
Andres Laguilavo <lagui2003@gmail.com>
2023-03-18 13:37:19 -0500

    update linkCustom and GithubIcon

--- src/components/LinkCustom/index.js
+++ src/components/LinkCustom/index.js
@@ -3,7 +3,8 @@ import React from 'react'
 
 export default function LinkCustom({ children, href, styles = '' }) {
   return (
-    <Link href={href} target='_blank' className={`flex items-center gap-4 rounded-md border-primary-300 p-2 text-xl text-primary-300 transition-all duration-100 ease-in-out hover:bg-secondary-600 hover:scale-110 ${styles}`}>
+    // <Link href={href} target='_blank' className={`flex items-center gap-3 rounded-md border-primary-300 p-2 text-xl text-primary-300 transition-all duration-100 ease-in-out hover:bg-secondary-600 hover:scale-110 ${styles}`}>
+    <Link href={href} target='_blank' className={`flex items-center gap-3 rounded-md  p-2 text-xl text-primary-200 transition-all duration-100 ease-in-out border-2 border-transparent hover:border-secondary-500 hover:scale-110 ${styles}`}>
       {children}
     </Link>
   )
