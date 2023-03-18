7756369f47e06079339bbb55ff682e17e7d686ae
Andres Laguilavo <lagui2003@gmail.com>
2023-03-18 13:57:11 -0500

    fix topbar in mobile

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -2,10 +2,10 @@
 export function TopBar() {
   return (
     <>
-      <header className='fixed top-0 left-0  right-0 z-30 w-full '>
-        <nav className='flex items-center justify-between py-2 px-4 bg-transparent backdrop-blur-[20px]'>
+      <header className='fixed top-0 left-0  right-0 z-30 w-full'>
+        <nav className='flex items-center justify-between py-2 px-4  backdrop-blur-[20px]'>
           <p className='text-xl hidden sm:flex'>Andres Laguilavo</p>
-          <ul className='flex gap-8 justify-center w-full sm:w-auto '>
+          <ul className='flex gap-8 justify-center w-full sm:w-auto text-xs sm:text-base text-center'>
             <li><a href='#about'>Sobre Mi</a></li>
             <li><a href='#skills'>Habilidades</a></li>
             <li><a href='#projects'>Proyectos</a></li>
