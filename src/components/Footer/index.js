0e0a7800e307039549ba95cfbbeed5c0f331a9ec
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-06-21 13:11:15 -0500

    fiix: change colors footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,11 +3,11 @@ import React from 'react'
 
 export function Footer() {
   return (
-    <footer className='absolute bottom-5 right-0 left-0 m-auto flex w-fit justify-self-center text-center text-primary-200'>
+    <footer className='absolute bottom-5 right-0 left-0 m-auto flex w-fit justify-self-center text-center text-primary-300'>
       Made with ❤️ by
       <Link
         href='https://github.com/Laguilavo10'
-        className='pl-1 border-b border-dashed border-black transition-colors duration-200 ease-in hover:border-orange-400 hover:text-orange-400'>
+        className='text-white pl-1 border-b border-dashed border-white transition-colors duration-200 ease-in hover:border-orange-400 hover:text-orange-400'>
         @laguilavo10
       </Link>
     </footer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import { TopBar } from '@components/TopBar'
 
 export default function Home({ images }) {
   return (
-    <main className='m-auto flex min-h-screen max-w-[1440px] flex-col gap-10 bg-primary-500 p-5 font-poppins text-primary-200 sm:px-20 relative pb-20'>
+    <main className='m-auto flex min-h-screen max-w-[1440px] flex-col gap-10 bg-primary-500 p-5 font-poppins text-primary-200 sm:px-20 relative pb-28'>
       <TopBar />
       <Profile />
       <AboutMe />
