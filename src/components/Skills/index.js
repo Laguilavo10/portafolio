1972321cd3ed9a17e3512621de049c32014c2eed
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-07-05 13:31:00 -0500

    feat: add colors svg skills on hover

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -16,43 +16,53 @@ import { useLanguaje } from 'context/useLanguaje'
 const Stack = [
   {
     name: 'HTML',
-    icon: <HtmlIcon height='70' width='70' />
+    icon: <HtmlIcon height='70' width='70' />,
+    color: '[&>svg]:hover:fill-[#E34F26]'
   },
   {
     name: 'CSS',
-    icon: <CssIcon height='70' width='70' />
+    icon: <CssIcon height='70' width='70' />,
+    color: '[&>svg]:hover:fill-[#1572B6]'
   },
   {
     name: 'JavaScript',
-    icon: <JavascriptIcon height='70' width='70' />
+    icon: <JavascriptIcon height='70' width='70' />,
+    color: '[&>svg]:hover:fill-[#F7DF1E]'
   },
   {
     name: 'TypeScript',
-    icon: <TypescriptIcon height='70' width='70' />
+    icon: <TypescriptIcon height='70' width='70' />,
+    color: '[&>svg]:hover:fill-[#007ACC]'
   },
   {
     name: 'React',
-    icon: <ReactIcon height='70' width='70' />
+    icon: <ReactIcon height='70' width='70' />,
+    color: '[&>svg]:hover:fill-[#61DAFB]'
   },
   {
     name: 'TailwindCSS',
-    icon: <TailwindIcon height='70' width='70' />
+    icon: <TailwindIcon height='70' width='70' />,
+    color: '[&>svg]:hover:fill-[#06B6D4]'
   },
   {
     name: 'Git',
-    icon: <GitIcon height='70' width='70' />
+    icon: <GitIcon height='70' width='70' />,
+    color: '[&>svg]:hover:fill-[#F05032]'
   },
   {
     name: 'NextJS',
-    icon: <NextjsIcon height='70' width='70' />
+    icon: <NextjsIcon height='70' width='70' />,
+    color: '[&>svg]:hover:fill-white'
   },
   {
     name: 'SQL',
-    icon: <SQLIcon height='70' width='70' />
+    icon: <SQLIcon height='70' width='70' />,
+    color: '[&>svg]:hover:fill-yellow-500'
   },
   {
     name: 'GraphQL',
-    icon: <GraphQLIcon height='70' width='70' />
+    icon: <GraphQLIcon height='70' width='70' />,
+    color: '[&>svg]:hover:fill-[#E10098]'
   }
 ]
 export function Skills() {
@@ -61,12 +71,11 @@ export function Skills() {
     <>
       <section id='skills' className='pt-16'>
         <TitleSection>{lang.skills.title}</TitleSection>
-        <ul className='my-10 flex grid-cols-5 justify-between gap-4 sm:grid flex-wrap'>
-          {Stack.map(({ name, icon }) => (
+        <ul className='my-10 flex grid-cols-5 flex-wrap justify-center gap-4 sm:grid'>
+          {Stack.map(({ name, icon, color = 'white' }) => (
             <li
               key={name}
-              className='flex flex-col items-center gap-2 transition-all duration-100 ease-out hover:scale-125'
-            >
+              className={`flex flex-col items-center gap-2 transition-all duration-100 ease-out hover:scale-125 ${color} [&>svg]:fill-white`}>
               {icon}
               <span className='text-l text-primary-300'>{name}</span>
             </li>
