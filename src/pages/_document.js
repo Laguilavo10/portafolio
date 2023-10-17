8f526526c2853cb31ce051704d816c1de280f5de
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-10-17 18:14:52 -0500

    add: add og metadata

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,9 +4,24 @@ export default function Document() {
   return (
     <Html>
       <Head>
-      <meta name="description" content="Apasionado Frontend Developer con experiencia en la creación de aplicaciones web modernas y atractivas."/>
-      <meta name="keywords" content="Frontend Developer, HTML, CSS, JavaScript, React, TailwindCSS, NextJS"/>
-      <meta name="author" content="Andres Felipe Laguilavo Ruiz"/>
+        <meta
+          name='description'
+          content='Apasionado Frontend Developer con experiencia en la creación de aplicaciones web modernas y atractivas.'
+        />
+        <meta
+          name='keywords'
+          content='Frontend Developer, HTML, CSS, JavaScript, React, TailwindCSS, NextJS'
+        />
+        <meta name='author' content='Andres Felipe Laguilavo Ruiz' />
+        <meta property='og:title' content='Andres Felipe Laguilavo Ruiz - Software Developer' />
+        <meta
+          property='og:description'
+          content='Desarrollador de Software con enfasis en las tecnologias Web. Me interesan especialmente los temas sociales y me gusta contribuir con soluciones a los problemas a través de la tecnología.Con experiencia en el desarrollo de aplicaciones web escalables y sitios interactivos.'
+        />
+        <meta
+          property='og:image'
+          content='/cover-link.png'
+        />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
           rel='preconnect'
@@ -19,7 +34,7 @@ export default function Document() {
         />
         <link rel='shortcut icon' href='/favicon.svg' type='image/x-icon' />
       </Head>
-      <body className='bg-primary-500 overflow-x-hidden'>
+      <body className='overflow-x-hidden bg-primary-500'>
         <Main />
         <NextScript />
       </body>
