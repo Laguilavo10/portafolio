016172717b5c065468bc70696384e2886dec8ba7
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-12-02 14:38:27 -0500

    Update API request to filter images by aspect-ratio

--- src/pages/api/get-certifications.js
+++ src/pages/api/get-certifications.js
@@ -8,14 +8,17 @@ export default async function handler(req, res) {
           `${process.env.API_KEY}:${process.env.API_SECRET}`
         ).toString('base64')})}`
       },
-      body: JSON.stringify({ expression: 'folder=Certifications/lagui2003@gmail.com' })
+      body: JSON.stringify({
+        expression:
+          'folder=Certifications/lagui2003@gmail.com AND aspect_ratio>=1',
+        max_results: 5
+      })
     }
 
     try {
       const data = await fetch(process.env.URL_API, options)
       const response = await data.json()
-      const images = response.resources.splice(0, 5)
-      res.status(200).json({ data: images })
+      res.status(200).json({ data: response.resources })
     } catch (error) {
       console.log(error)
       res.status(500).json({ message: error })
