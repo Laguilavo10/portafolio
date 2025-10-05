2e12b1e875ad4c08cbce99291d1dde1315003d42
Laguilavo10 <lagui2003@gmail.com>
2025-10-05 14:55:43 -0500

    Replace profile image and remove unused image file

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,9 +1,8 @@
-// import { useState } from 'react'
 import { TitleSection } from '@components/TitleSection'
 import Image from 'next/image'
-// import cara from '../../../public/cara.jpg'
-import cara from '../../../public/cara2.jpg'
+import fotoPerfil from '../../../public/foto_perfil.png'
 import { useLanguaje } from 'context/useLanguaje'
+
 export function AboutMe() {
   const { lang } = useLanguaje()
   return (
@@ -11,7 +10,7 @@ export function AboutMe() {
       <section id='about' className='m-auto pt-12 lg:pt-24'>
         <div className='relative flex max-w-[1200px] flex-col items-center gap-14 rounded-md p-8 md:grid-cols-5 lg:grid lg:bg-primary-400/50'>
           <Image
-            src={cara.src}
+            src={fotoPerfil.src}
             width='1000'
             height='1000'
             className='aspect-square w-72 rounded-lg object-cover lg:absolute lg:left-10 xl:w-96'
